f6982d36218252003657c47e1d74ee222cc34a43
ShahzadUmarBaig <ShahzadUBaig@gmail.com>
2021-05-10 22:38:07 +0500

    moved API Key

--- routes/notification.js
+++ routes/notification.js
@@ -3,10 +3,6 @@ const router = express.Router();
 const gcm = require("node-gcm");
 const Notifications = require("./../models/Notifications");
 
-var sender = new gcm.Sender(
-  "AAAA71p0hGQ:APA91bHdT4Huz9V3_pf_nFtsqbA-hsafXFyjDqAZqYnZ4gmoXyEkE4mBOafm4He9Q2Jie_HPNiOH1vC5mEepxtvibm7XFLaoeMvAQD_HRyCWhlD-B0RlleXilxiFANE8GhKSsW6In8_c"
-);
-
 router.post("/register-device", (req, res, next) => {
   Notifications.create(req.body)
     .then((Notifications) => {
@@ -43,6 +39,10 @@ sendNotificationAndroid = (msg, devicesIds) => {
 
   // Specify which registration IDs to deliver the message to
   // var regTokens = ['elE6mbp1ORHw4o_BHcuncn:APA91bG8um-W6yTU7wzz_kwt7J7pothl48To4ac3OE_kOs08PmoYOjRxFP_68Z7dWlGlztoUNiwN3A6RRPT4-LnPz7pfTY9OtyUk3XGa8eYhiVGpeC_LV-mB6wruAjj6pbuaU8ELIVPc'];
+
+  var sender = new gcm.Sender(
+    "AAAA71p0hGQ:APA91bHdT4Huz9V3_pf_nFtsqbA-hsafXFyjDqAZqYnZ4gmoXyEkE4mBOafm4He9Q2Jie_HPNiOH1vC5mEepxtvibm7XFLaoeMvAQD_HRyCWhlD-B0RlleXilxiFANE8GhKSsW6In8_c"
+  );
 
   // Actually send the message
   sender.send(
